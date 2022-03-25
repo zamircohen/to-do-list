be731ad8412be8fe939b17188f5a803f107b8181
Zamir Cohen <90824610+zamircohen@users.noreply.github.com>
2022-03-25 18:44:31 +0100

    Created todo model and log in function in index.js

--- server/index.js
+++ server/index.js
@@ -5,8 +5,29 @@ const { User } = require("./models/user")
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+const JWT_SECRET = "0823uoiwehfFusTKLciadfsbaasd2346sdfbjaenrw"
 
+// MIDDLEWARES
 app.use(express.json())
+
+app.use((req, res, next) => {
+  const authHeader = req.header("Authorization")
+  if (authHeader) {
+    const token = authHeader.split(" ")[1]
+    req.user = jwt.verify(token, JWT_SECRET)
+  }
+  next()
+})
+
+
+const requireLogin = (req, res, next) => {
+  if (req.user) {
+      next();
+  } else {
+      res.sendStatus(401);
+  }
+}
+
 
 
 
@@ -15,8 +36,36 @@ app.get("/api", (req, res) => {
     res.json({ message: "Your TO-DO list" });
   });
 
+  
+
+// TEST - SECRET PAGE THAT SHOWS ONLY IF A USER IS LOGGED IN
+app.get("/secret", requireLogin, (req, res) => {
+    res.json({message: `Hello again ${req.user.username}`})
+})
 
 
+
+// LOGIN 
+app.post("/login", async (req, res) => {
+    const {username, password} = req.body
+    const user = await User.login(username, password)
+    if (user) {
+      const userId = user._id.toString()
+      const token = jwt.sign(
+        {userId, username: user.username},
+        JWT_SECRET,
+        {expiresIn: "1h", subject: userId}
+      )
+      res.json({token})
+      
+    } else {
+      res.sendStatus(401)
+    }
+})
+
+
+
+// CREATE USER
 app.post("/users", async (req, res) => {
   const {username, password} = req.body
   const user = new User({username, password})
---
+++ server/models/todo.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose")
+
+const todoSchema = new mongoose.Schema({
+    todo: {type: String},
+    user: {type: mongoose.Schema.ObjectId, ref: "User"},
+    date: {type: Date, default: Date.now},
+    done: {type: Boolean}
+})
+
+const Todo = mongoose.model("Todo", todoSchema)
+exports.Todo = Todo
