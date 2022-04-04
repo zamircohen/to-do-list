10c58dfe690c6ace9b1b45bcf6797a3ccb7a00b5
Zamir Cohen <90824610+zamircohen@users.noreply.github.com>
2022-04-04 15:43:30 +0200

    Added a Container component and implemented css

---
+++ client/src/components/Container.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import styled from 'styled-components'
+
+
+const StyledContainer = styled.div`
+    width: 25%;
+    height: 300px;
+    margin: auto;
+    margin-top: 15%;
+    padding: 10px;
+    background-color: lightgray;
+    text-align: center;
+
+    // div {
+    //     box-shadow: 10px 10px;
+    //   }
+`
+
+export default StyledContainer
+
+// export default function Container() {
+//   return (
+//     <div>
+//         <StyledContainer>
+
+//         </StyledContainer>
+//     </div>
+//   )
+// }
